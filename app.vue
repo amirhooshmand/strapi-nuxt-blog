4a86e95897454fa06cf531e97077272c63d8948a
Amir.H Hooshmand <amir.hsmart@gmail.com>
2024-11-09 15:08:15 +0330

    feat: update default layout and SEO setup
    - Added `default.vue` layout to include `BaseNavbar` and `BaseFooter` with dynamic content slot.
    - Implemented global SEO settings in `app.vue` to set meta tags and favicon dynamically from Strapi.
    - Improved body styling with background color and minimum height applied globally.

--- app.vue
+++ app.vue
@@ -1,10 +1,51 @@
-<script setup>
-console.log("s");
+<script setup lang="ts">
+import type { IGlobal } from "~/types/global";
+import { useStrapiMedia } from "~/composables/useStrapiMedia";
+
+const { findOne } = useStrapi();
+const globalResponse = await findOne<IGlobal>("global", {
+  populate: {
+    defaultSeo: {
+      populate: "*",
+    },
+    favicon: {
+      populate: "*",
+    },
+  },
+});
+const global = globalResponse.data;
+
+useHead({
+  title: global?.defaultSeo.metaTitle,
+  htmlAttrs: { lang: "en" },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: useStrapiMedia(global?.favicon?.url),
+    },
+  ],
+  meta: [
+    { name: "description", content: global?.defaultSeo?.metaDescription },
+    { name: "og:title", content: global?.defaultSeo?.metaTitle },
+    { name: "og:description", content: global?.defaultSeo?.metaDescription },
+    { name: "og:type", content: "website" },
+    { name: "og:locale", content: "en_US" },
+    {
+      name: "og:image",
+      content: useStrapiMedia(global?.defaultSeo?.shareImage.url),
+    },
+  ],
+});
 </script>
 
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <Html>
+    <Body>
+      <NuxtLayout>
+        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex-1">
+    <BaseNavbar />
+    <main class="min-h-[calc(100vh-var(--footer-height))]">
+      <div class="">
+        <slot />
+      </div>
+    </main>
+    <BaseFooter />
+  </div>
+</template>
+
+<style lang="scss">
+:root {
+  --footer-height: 57px;
+}
+</style>
